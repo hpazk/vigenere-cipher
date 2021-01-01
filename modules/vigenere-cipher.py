2e0347279d27b576056ded1015f0c829e92e7a13
hpazk <hpazk.zkh@gmail.com>
2021-01-01 08:33:45 +0700

    init project

---
+++ modules/vigenere-cipher.py
@@ -0,0 +1,13 @@
+class VigenereChiper:
+    def __init__(self, text, key):
+        self.text = text
+        self.key = key
+
+    def key_generator(self):
+        pass
+
+    def encryptor(self):
+        pass
+
+    def decryptor(self):
+        pass
